87d56bc6bca60c8f8104c6b5afd015d4524d340a
santoshthapa321 <santosh.thpa.2080@gmail.com>
2024-05-12 08:13:57 +0545

    added

---
+++ index.js
@@ -0,0 +1,49 @@
+console.log(
+    "hello");
+//variable....
+//container / box -means esle kei kura store garchha
+// variables store data
+greetings = "Namaste";
+name = "Santosh";
+fullname = "Santosh Thapa"
+age = 22;
+console.log(greetings);
+console.log(name);
+console.log(age);
+console.log(greetings, name, age);
+console.log(fullname);
+//reserved keywords
+// for="fff";
+// if="nbkdvb"
+// const="gg"
+eutaofficekoaddress = "balaju"; //no space
+euta_office_ko_address = "Sukedhara";//snake case
+eutaOfficeKoAddress = "dallukomassage center"//camel case
+console.log(euta_office_ko_address, eutaOfficeKoAddress, eutaofficekoaddress
+)
+//variables ma kk kura rakhna milxa teslai datatypes vanninxa
+productPrice= 100;
+tarkarikoprice = 100.5;
+ismarried= true;
+/*datatype
+1.string
+2.Number
+     integer
+     alost/double
+3. boolean
+    true/false
+
+*/
+//variables banaune practice haru
+//const
+//let
+//var sakbar nagarne
+var laptopBrand="Dell;" 
+console.log("laptop ko Brand", laptopBrand);
+const PI = 3.14;
+console.log("PI ko Value", PI);
+classTime = "1 Hour"
+console.log ( "class Time" classTime)
+
+
+  
