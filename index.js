48bf0e9b1ae5c2823c61688309a7f5d8a36fa097
santoshthapa321 <santosh.thpa.2080@gmail.com>
2024-05-13 09:00:58 +0545

    monday

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-console.log(
-    "hello");
+console.log("hello");
 //variable....
 //container / box -means esle kei kura store garchha
 // variables store data
 greetings = "Namaste";
 name = "Santosh";
-fullname = "Santosh Thapa"
+fullname = "Santosh Thapa";
 age = 22;
 console.log(greetings);
 console.log(name);
@@ -17,14 +16,13 @@ console.log(fullname);
 // if="nbkdvb"
 // const="gg"
 eutaofficekoaddress = "balaju"; //no space
-euta_office_ko_address = "Sukedhara";//snake case
-eutaOfficeKoAddress = "dallukomassage center"//camel case
-console.log(euta_office_ko_address, eutaOfficeKoAddress, eutaofficekoaddress
-)
+euta_office_ko_address = "Sukedhara"; //snake case
+eutaOfficeKoAddress = "dallukomassage center"; //camel case
+console.log(euta_office_ko_address, eutaOfficeKoAddress, eutaofficekoaddress);
 //variables ma kk kura rakhna milxa teslai datatypes vanninxa
-productPrice= 100;
+productPrice = 100;
 tarkarikoprice = 100.5;
-ismarried= true;
+ismarried = true;
 /*datatype
 1.string
 2.Number
@@ -38,18 +36,18 @@ ismarried= true;
 //const
 //let
 //var sakbar nagarne
-var laptopBrand="Dell" ;
+var laptopBrand = "Dell";
 console.log("laptop ko Brand", laptopBrand);
 const PI = 3.14;
 console.log("PI ko Value", PI);
 classTime = "1 Hour";
-console.log ( "class Time",classTime);
+console.log("class Time", classTime);
 //array example
 
-let subjects=["English", "Nepali", "Maths", "Computer"];
+let subjects = ["English", "Nepali", "Maths", "Computer"];
 // array contains similar data types
 let oppo = "oppo";
-let Brand = ["Samsung","Lenovo","Iphone",oppo];
+let Brand = ["Samsung", "Lenovo", "Iphone", oppo];
 console.log(Brand);
 //object concept
 /*
@@ -60,12 +58,94 @@ let object = {
 }
 */
 
-let schoolDetails={
-    schoolName:"Bagmati School",
-    schoolLoation:"Putalisadak",
-    schoolEstd:"1990",
-
+let schoolDetails = {
+  schoolName: "Bagmati School",
+  schoolLoation: "Putalisadak",
+  schoolEstd: "1990",
 };
 console.log(schoolDetails);
+//practice
+//1.Making object with keys and their values
 
-  
+let product = {
+  title: "t-Shirt",
+  color: "gray",
+  size: "XXL",
+  price: 999,
+  discount: "10%",
+};
+//2. Practicing array
+let yellow = "purple";
+const colors = ["red", "blue", "green", yellow];
+//3. Practicing array inside objects
+const cars = {
+  BMW: ["ix1", "ixx"],
+  suzuki: ["alto", "breeza"],
+  toyata: ["camry", "crown"],
+};
+
+//printing 123
+console.log(cars, colors, product);
+//printing cars bmw only
+console.log(cars.BMW);
+//
+let a = 111 + 1;
+let b = "111" + 1;
+
+console.log("a=", a, "b=", b);
+// creating object room
+let room1 = {
+  color: ["blue", "white"],
+  dimensions: {
+    length: {
+      value: 10,
+      unit: "meter",
+    },
+    width: {
+      value: 5,
+      unit: "meter",
+    },
+    height: {
+      value: 3,
+      unit: "meter",
+    },
+  },
+  door_count: 1,
+};
+// teacher details here
+
+let teacher1 = {
+  fullname: "Sudip Paudel",
+  age: 30,
+  address: "kathmandu",
+  hobbies: ["coding", "singing"],
+  height: {
+    value: 6,
+    unit: "feet",
+  },
+  subjects: {
+    computer: {
+        subject1: "computer",
+      period: "1st",
+      class: "10",
+    },
+
+    english: {
+      period: "5th",
+      class: 9,
+    },
+  },
+};
+//printing random keys
+console.log(teacher1.fullname, teacher1.address);
+// printing sudip paudel ko 1st period class 10 ma computer chha
+console.log(
+  teacher1.fullname +
+    " ko " +
+    teacher1.subjects.computer.period +
+    " period class " +
+    teacher1.subjects.computer.class +
+    " ma " +
+    teacher1.subjects.computer.subject1 +
+    " chha"
+);
